22f39fa40a3a0e4205d5cc68e9fbaaa53fa9b53a
MarcioBADias <98918078+MarcioBADias@users.noreply.github.com>
2023-01-11 08:39:42 -0300

    Acerto da caixa infos c/ map no CSS usando z-index

--- app/app.js
+++ app/app.js
@@ -7,6 +7,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+console.log(L.tileLayer())
+
 form.addEventListener('submit', e => {
     e.preventDefault();
     const inputValue = form.input.value;

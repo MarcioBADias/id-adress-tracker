ced49ac59c1f30f75654759c6d65aad6bb9f9e36
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-01-12 00:27:42 -0300

    Convertendo o JSON do API

--- app/app.js
+++ app/app.js
@@ -7,7 +7,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-console.log(L.tileLayer())
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -18,3 +17,29 @@ form.addEventListener('submit', e => {
     }
     return spanResponse.textContent = `Você escreveu ${inputValue}`;
 })
+
+const getGeoIp = callback => {
+    const request = new XMLHttpRequest();
+        
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200){
+            const data = JSON.parse(request.responseText);
+            callback(null,data);
+            return
+        }
+        if(request.readyState === 4){
+            return console.log('Dados não retornaram',null)
+        }
+    })
+    request.open('GET', 'https://geo.ipify.org/api/v2/country?apiKey=at_8j2YtcS1sRr6olfsrjR4iaPdHNn7o&ipAddress=8.8.8.8');
+    request.send();
+}
+
+getGeoIp((error,data)=>{
+    if(error){
+        return error;
+    }
+    return console.log(data);
+})
+
+

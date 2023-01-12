c8eb3a5d9fe51f04d74d708646712e2c9b0ff9d0
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-01-12 09:08:55 -0300

    Realizando testes e ajustes na API e localização

--- app/app.js
+++ app/app.js
@@ -1,26 +1,19 @@
 const form = document.querySelector('form');
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+// CRIAÇÃO DO MAPA COM LEAFLET
+
+var map = L.map('map')
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const inputValue = form.input.value;
-    const spanResponse = form.querySelector('span');
-    if(inputValue === ''){
-        return spanResponse.textContent = `Isira um valor acima`;
-    }
-    return spanResponse.textContent = `Você escreveu ${inputValue}`;
-})
+// API DE VERIFICAÇÃO DAS CORDENADAS E RETORNO
 
 const getGeoIp = callback => {
     const request = new XMLHttpRequest();
-        
+    
     request.addEventListener('readystatechange', () => {
         if(request.readyState === 4 && request.status === 200){
             const data = JSON.parse(request.responseText);
@@ -31,15 +24,38 @@ const getGeoIp = callback => {
             return console.log('Dados não retornaram',null)
         }
     })
-    request.open('GET', 'https://geo.ipify.org/api/v2/country?apiKey=at_8j2YtcS1sRr6olfsrjR4iaPdHNn7o&ipAddress=8.8.8.8');
+    request.open('GET', 'https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_8j2YtcS1sRr6olfsrjR4iaPdHNn7o&ipAddress=');
     request.send();
 }
+
+// CAPTAÇÃO DA POSIÇÃO DO USUÁRIO
 
 getGeoIp((error,data)=>{
     if(error){
         return error;
     }
-    return console.log(data);
+    const getData = data;
+    map.setView([getData.location.lat, getData.location.lng], 15);
+    return console.log(getData);
 })
 
 
+// const success = position => {
+//     console.log(position)
+//     return 
+// }
+
+// navigator.geolocation.watchPosition(success)
+
+
+// CAPITURA DO FORM
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const inputValue = form.input.value;
+    const spanResponse = form.querySelector('span');
+    if(inputValue === ''){
+        return spanResponse.textContent = `Isira um valor acima`;
+    }
+    return spanResponse.textContent = `Você escreveu ${inputValue}`;
+})
